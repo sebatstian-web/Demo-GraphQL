0a0d30185c25c40d4e328fee7374fc53bb32ef4f
Sebastián <sebtstian@gmail.com>
2020-08-22 14:38:52 -0300

    obteniendo info por estudiante mediante id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -8,6 +8,24 @@ const query: IResolvers = {
     estudiantes(): any {
       return db.estudiantes;
     },
+    estudiante(__: void, { id }): any {
+      // [0] Permite obtener el primer resultado, o dará error
+      const data = db.estudiantes.filter(
+        (estudiante) => estudiante.id === id
+      )[0];
+
+      if (!data) {
+        // Para enviar un error hay que respetar la estructura del objeto
+        return {
+          id: '-1',
+          name: `Estudiante con ID ${id}, no existe`,
+          email: '',
+          courses: [],
+        };
+      }
+
+      return data;
+    },
   },
 };
 
