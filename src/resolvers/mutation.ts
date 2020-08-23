a54fdd1cd177c8cb446b5631b372a5dc4874d53d
Sebastián <sebtstian@gmail.com>
2020-08-23 19:07:25 -0300

    agregada Mutation para ingresar nuevo curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -3,6 +3,26 @@ import _ from 'lodash';
 
 import { db } from '../data/data.store';
 
-const mutation: IResolvers = {};
+const mutation: IResolvers = {
+  Mutation: {
+    nuevoCurso(__: void, { curso }): any {
+      const itemCurso = {
+        id: String(db.cursos.length + 1),
+        title: curso.title,
+        description: curso.description,
+        clases: curso.clases,
+        time: curso.time,
+        level: curso.level,
+        logo: curso.logo,
+        path: curso.path,
+        teacher: curso.teacher,
+        reviews: [],
+      };
+
+      db.cursos.push(itemCurso);
+      return itemCurso;
+    },
+  },
+};
 
 export default mutation;
